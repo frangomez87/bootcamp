8868c4f92f01afed24a08800b38b94f022fa171f
francisco gomez <javi.gomez87@hotmail.com>
2019-07-17 15:12:06 -0400

    archivo Dino

---
+++ clases/dino1.py
@@ -0,0 +1,21 @@
+class Dino:
+    ojos = 2
+    def __init__(self, un_nombre, un_color, cantidad_patas=4, un_genero=None):
+        self.nombre = un_nombre
+        self.color = un_color
+        self.patas = cantidad_patas
+        self.genero = un_genero
+        print("naci")
+
+    def saludar(self):
+        print("hola me llamo", self.nombre, "tengo", self.patas, "patas y soy", self.genero)
+
+    def cortar_pata(self, cantidad_patas_a_cortar=1):
+        self.patas = self.patas - cantidad_patas_a_cortar
+
+    def decir_genero(self):
+        print("hola soy", self.nombre, "y me identifico como", self.genero)
+
+pepe = Dino("pepito", "verde",4,"macho alfa pecho peludo")
+print(pepe.nombre)
+pepe.saludar()
