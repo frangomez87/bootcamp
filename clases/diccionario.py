2e2d9f283f52c0de344d6380f9c58a65f1ffb296
francisco gomez <javi.gomez87@hotmail.com>
2019-07-18 09:18:54 -0400

    archivo diccionario

---
+++ clases/diccionario.py
@@ -0,0 +1,11 @@
+nombre_de_diccionario = {"nombre de la clave":"valor de cualquiera"}
+diccionario_vacio = {}
+dic_persona = {"0"}
+nombre = "steven"
+lista_hobbies = ["rugby", "comer mbeju", "cortar patas de dinos"]
+idiomas = {
+    "espanol":"bueno"
+    "ingles":"bueno"
+    "guarani":"jopara"
+    "portuguez":"kaure"
+}
